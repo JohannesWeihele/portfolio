466864136eb73117dec672abf509f9038fce58c5
Johannes Weihele <johannes.weihele@hs-augsburg.de>
2023-05-24 14:54:46 +0200

    - add responsive design for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Main_Body from "./components/main_body/main_body";
 const App = () => {
   return (
       <div className="App">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
           <Header/>
           <Main_Body/>
       </div>
