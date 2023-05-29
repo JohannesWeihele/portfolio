a2b103f154287832815c7c514cfd0f11720320c0
Johannes Weihele <johannes.weihele@hs-augsburg.de>
2023-05-29 19:06:17 +0200

    - readded attic with lower details and without normal maps

--- src/components/3D/dachboden.tsx
+++ src/components/3D/dachboden.tsx
@@ -1,41 +1,55 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Color } from 'three';
-import {Canvas, useFrame, useLoader, useThree} from '@react-three/fiber';
-import { OrbitControls, useGLTF } from '@react-three/drei';
+import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber';
+import { OrbitControls, useGLTF, Loader, Html, useProgress } from '@react-three/drei';
+
+const LoadingScreen: React.FC = () => {
+    const { progress } = useProgress();
+
+    return (
+        <Html center>
+            <div>Lade Dachboden...</div>
+            <div>{Math.round(progress)}%</div>
+            <div style={{ width: '100%', height: '10px', background: '#ccc' }}>
+                <div style={{ width: `${progress}%`, height: '100%', background: '#0f0' }} />
+            </div>
+        </Html>
+    );
+};
 
 const Dachboden: React.FC = () => {
     const { scene } = useGLTF('/models/dachboden/dachboden.gltf');
-
-    const { camera } = useThree();
-    // Setze den Zoomwert
-    if (window.innerWidth <= 768) {
-
-        camera.zoom = 0.4;
-
-        camera.updateProjectionMatrix();
-    } else {
-        camera.zoom = 1.0;
-
-        camera.updateProjectionMatrix();
-    }
+    const isLoading = !scene;
 
     return (
-        <group position={[0,-1.5,0.1]}>
-            {/* Füge den Inhalt der GLTF-Szene hinzu */}
-            <ambientLight intensity={0.5} color="white" />
-            <primitive object={scene}/>
-            {/* Füge die OrbitControls hinzu, um das Modell zu steuern */}
-            <OrbitControls
-                enableZoom={true}
-                enablePan={true}
-                enableRotate={true}
-                zoomSpeed={0.5}
-                target={[0, 15, -45]} // Setze den Kameraziel-Punkt auf den Ursprung der Szene
-                minDistance={0} // Minimale Zoom-Entfernung
-                maxDistance={7} // Maximale Zoom-Entfernung
-            />
-            <directionalLight castShadow position={[0, 10, 0]} intensity={0.5} shadow-mapSize-width={1024} shadow-mapSize-height={1024} />
-        </group>
+        <>
+            {isLoading ? (
+                <LoadingScreen />
+            ) : (
+                <group position={[0, -1.5, 0.1]}>
+                    {/* Füge den Inhalt der GLTF-Szene hinzu */}
+                    <ambientLight intensity={0.5} color="white" />
+                    <primitive object={scene} />
+                    {/* Füge die OrbitControls hinzu, um das Modell zu steuern */}
+                    <OrbitControls
+                        enableZoom={true}
+                        enablePan={true}
+                        enableRotate={true}
+                        zoomSpeed={0.5}
+                        target={[0, 15, -45]} // Setze den Kameraziel-Punkt auf den Ursprung der Szene
+                        minDistance={0} // Minimale Zoom-Entfernung
+                        maxDistance={7} // Maximale Zoom-Entfernung
+                    />
+                    <directionalLight
+                        castShadow
+                        position={[0, 10, 0]}
+                        intensity={0.5}
+                        shadow-mapSize-width={1024}
+                        shadow-mapSize-height={1024}
+                    />
+                </group>
+            )}
+        </>
     );
 };
 
