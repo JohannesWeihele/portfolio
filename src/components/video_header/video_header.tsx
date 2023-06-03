47b8153d96b1eb264aedc9874117ed5bb3703cd6
Johannes Weihele <johannes.weihele@hs-augsburg.de>
2023-06-03 12:00:25 +0200

    - added background-picture

--- src/components/video_header/video_header.tsx
+++ src/components/video_header/video_header.tsx
@@ -4,6 +4,8 @@ import { useMediaQuery } from 'react-responsive';
 import './video_header.css';
 
 import fullscreen_icon from '../../resources/icons/fullscreen_icon.png'
+import player_icon from '../../resources/icons/player_button.png'
+import pause_icon from '../../resources/icons/pause_button.png'
 
 interface VideoHeaderProps {
     src: string;
@@ -13,10 +15,13 @@ interface VideoHeaderProps {
 
 const VideoHeader: FC<VideoHeaderProps> = ({ src , showFullscreenButton= true, zoomed = true}) => {
     const videoRef = useRef<HTMLVideoElement>(null);
+    const imgRef = useRef<HTMLImageElement>(null);
     const isMobile = useMediaQuery({ maxWidth: 767 });
     const [isLoading, setIsLoading] = useState(true);
     const [isFullscreen, setIsFullscreen] = useState(false);
+    const [isPauseVideo, setIsPauseVideo] = useState(true);
     let zoomed_class: string = '';
+    let initial_play: boolean = false;
 
     if(!zoomed){
         zoomed_class = 'no_zoomed_video';
@@ -26,6 +31,9 @@ const VideoHeader: FC<VideoHeaderProps> = ({ src , showFullscreenButton= true, z
         const element = videoRef.current;
         if (!element) return;
 
+        const img = imgRef.current;
+        if (!img) return;
+
         const elementPosition = element.getBoundingClientRect().top;
         const windowHeight = window.innerHeight;
         const videoHeight = element.offsetHeight;
@@ -34,9 +42,14 @@ const VideoHeader: FC<VideoHeaderProps> = ({ src , showFullscreenButton= true, z
         const visibleArea = windowHeight - elementPosition;
 
         if (visibleArea >= videoHeight * 0.5) {
-            element.play();
+            if(initial_play){
+                element.play();
+                img.className ='item_gone';
+            }
         } else {
-            element.pause();
+            if(initial_play){
+                element.pause();
+            }
         }
     };
 
@@ -55,9 +68,37 @@ const VideoHeader: FC<VideoHeaderProps> = ({ src , showFullscreenButton= true, z
         }
     };
 
+
     const handleLoadedData = () => {
         setIsLoading(false);
     };
+
+    const handlePlayerClick = () => {
+        const element = videoRef.current;
+        if (!element) return;
+        if(isPauseVideo){
+            setIsPauseVideo(false);
+            element.play()
+            if(!initial_play){
+                initial_play = true;
+            }
+        } else {
+            setIsPauseVideo(true);
+            element.pause();
+        }
+    }
+
+    const handleMouseEnter = () => {
+        const img = imgRef.current;
+        if (!img) return;
+        img.className ='item_visible';
+    }
+
+    const handleMouseLeave = () => {
+        const img = imgRef.current;
+        if (!img) return;
+        img.className ='item_gone';
+    }
 
     useEffect(() => {
         const element = videoRef.current;
@@ -101,6 +142,8 @@ const VideoHeader: FC<VideoHeaderProps> = ({ src , showFullscreenButton= true, z
             <div className={'video_container'}>
                 <div>
                     <video
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
                         ref={videoRef}
                         className={isMobile ? 'mobile' : `desktop ${zoomed_class}`}
                         muted
@@ -112,12 +155,20 @@ const VideoHeader: FC<VideoHeaderProps> = ({ src , showFullscreenButton= true, z
                         <source src={src} type="video/mp4" />
                     </video>
                 </div>
-                {showFullscreenButton && ( // Überprüfe die Prop, um den Button anzuzeigen
-                    <button className="fullscreen-button" onClick={toggleFullscreen}>
-                        {isFullscreen ? 'Exit Fullscreen' : ''}
-                        <img src={fullscreen_icon} alt="Fullscreen" />
-                    </button>
+                <div>
+                    {showFullscreenButton && ( // Überprüfe die Prop, um den Button anzuzeigen
+                        <button className="fullscreen-button" onClick={toggleFullscreen}>
+                            {isFullscreen ? 'Exit Fullscreen' : ''}
+                            <img src={fullscreen_icon} alt="Fullscreen" />
+                        </button>
                     )}
+                </div>
+                <div className={"player_button"} onClick={handlePlayerClick}>
+                    <img
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                        ref={imgRef} src={isPauseVideo ? player_icon : pause_icon}/>
+                </div>
             </div>
         </div>
     );
