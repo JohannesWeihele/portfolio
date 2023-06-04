aaf44d0a91480fb72408c8a3e2797f7eeca3d4ff
Johannes Weihele <johannes.weihele@hs-augsburg.de>
2023-06-05 00:27:26 +0200

    - added background-picture

--- src/components/main_body/main_body.tsx
+++ src/components/main_body/main_body.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Portrait from "./portrait";
 import Introduction from "./introduction";
 import landscape_image from "../../resources/images/backgrounds/landscape.png"
@@ -7,35 +7,47 @@ import landscape_front_mirrored from "../../resources/images/backgrounds/landsca
 import "./main_body.css"
 import Projects from "./projects/projects";
 import { Parallax } from 'react-parallax';
-import { ParallaxBanner} from "react-scroll-parallax";
+import { ParallaxBanner } from "react-scroll-parallax";
 import Tilt from "react-parallax-tilt";
 
-const Main_Body: React.FC = () =>{
+const Main_Body: React.FC = () => {
+    const [isMobileChecked, setIsMobileChecked] = useState(false);
+    const [isMobileView, setIsMobileView] = useState(false);
+    const [parallaxStrength, setParallaxStrength] = useState(700);
 
+    useEffect(() => {
+        if (!isMobileChecked) {
+            setIsMobileChecked(true);
+            setIsMobileView(window.innerWidth <= 768); // Set the threshold based on your design
+        }
 
+        setParallaxStrength(isMobileView ? 100 : 700);
+    }, [isMobileChecked, isMobileView]);
 
-    return <div className={"main_body"}>
-        <Parallax bgImage={landscape_image} strength={400}>
-            <ParallaxBanner
-                layers={[
-                    //{image: landscape_image, speed: 10, scale: [0.9, 1.5, 'easeIn']},
-                    {image: landscape_front_mirrored, speed: 0, scale: [1, 1.5, 'easeIn']},
-                    {image: landscape_front_mirrored, speed: 35, scale: [1, 1.5, 'easeIn']},
-                    {image: landscape_front, speed: 50, scale: [1, 1.5, 'easeIn'] },
-                ]}>
-            <div style={{height: 1200}} className={"introduction_body"}>
-                <Tilt>
-                     <Portrait />
-                </Tilt>
-                <Introduction />
+    return (
+        <div className={"main_body"}>
+            <Parallax bgImage={landscape_image} strength={parallaxStrength} disabled={isMobileView}>
+                <ParallaxBanner
+                    layers={[
+                        { image: landscape_front_mirrored, speed: 0, scale: [1, 1.5, 'easeIn'] },
+                        { image: landscape_front_mirrored, speed: 35, scale: [1, 1.5, 'easeIn'] },
+                        { image: landscape_front, speed: 50, scale: [1, 1.5, 'easeIn'] },
+                    ]}
+                >
+                    <div style={{ height: 1200 }} className={"introduction_body"}>
+                        <Tilt>
+                            <Portrait />
+                        </Tilt>
+                        <Introduction />
+                    </div>
+                </ParallaxBanner>
+            </Parallax>
+            <div className="introduction_body"></div>
+            <div className={"projects_body"}>
+                <Projects />
             </div>
-            </ParallaxBanner>
-        </Parallax>
-            <div className="introduction_body">
-            </div>
-        <div className={"projects_body"}>
-            <Projects/>
         </div>
-    </div>
+    );
 }
-export default Main_Body
+
+export default Main_Body;
