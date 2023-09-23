941f54bd1d2e4ded4f645073042295ef162c2579
Johannes Weihele <j.weihele@web.de>
2023-09-23 20:46:58 +0200

    Fixed missing shopping-cart in angular/vue.js video

--- src/components/main_body/opener/opener.tsx
+++ src/components/main_body/opener/opener.tsx
@@ -27,7 +27,7 @@ const Opener: React.FC<OpenerProps> = () => {
         <div>
             <ParallaxBanner
                 layers={[
-                    { image: background, speed: 0, expanded: false, disabled: isMobileView},
+                    { image: background, speed: 0, translateY: [-35, 35], expanded: false, disabled: isMobileView},
                 ]}
             >
                 <div style={{ height: 1400 }} className={"introduction_body"}>
