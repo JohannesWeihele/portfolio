e44ce184d25b4c800c4bb2fe6c597ef728dfc1dc
Johannes Weihele <johannes.weihele@hs-augsburg.de>
2023-10-20 11:11:40 +0200

    final (probably not so final) commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,6 @@ import AboutMePage from "./pages/aboutme_page/aboutme_page";
 
 const App = () => {
 
-    useEffect(() => {
-
-        const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-
-        if (mediaQuery.matches) {
-            document.body.classList.add('light-mode');
-        }
-
-        return () => {
-            document.body.classList.remove('light-mode');
-        };
-    }, []);
-
-
     return (
       <Router>
           <Routes>
