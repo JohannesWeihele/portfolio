ea9f4d8ec99f451c15ff4fe69dfc8d478167fd02
Johannes Weihele <johannes.weihele@hs-augsburg.de>
2024-02-24 09:36:08 +0100

    updated Starting animation-time to 5sec. for reducing delay-time

--- src/components/main_body/opener/opener.tsx
+++ src/components/main_body/opener/opener.tsx
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Portrait from "../portrait";
 import Introduction from "../introduction";
-import landscape_background from "../../../resources/images/backgrounds/landscape.png"
-import landscape_front from "../../../resources/images/backgrounds/landscape_front.png"
-import landscape_front_mirrored from "../../../resources/images/backgrounds/landscape_front_mirrored.png"
 import background from "../../../resources/images/backgrounds/background.jpg"
 import "../main_body.css"
 import { ParallaxBanner } from "react-scroll-parallax";
