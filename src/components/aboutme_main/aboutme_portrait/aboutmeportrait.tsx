6746f919b68324b5ecd3e76ec36a982d439dca29
Johannes Weihele <johannes.weihele@hs-augsburg.de>
2025-10-12 11:39:51 +0200

    refactor(PORT-1): pre-load of image - pre-loading image

--- src/components/aboutme_main/aboutme_portrait/aboutmeportrait.tsx
+++ src/components/aboutme_main/aboutme_portrait/aboutmeportrait.tsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import './aboutmeportrait.css';
-import {Carousel} from "react-responsive-carousel";
-import bewerbungsbild from "../../../resources/portrait/bewerbungsbild.jpg"
-import portrait_two from "../../../resources/portrait/portrait_two.png"
-import huettenbild from "../../../resources/portrait/huette.jpeg"
-import office from "../../../resources/portrait/freizeit.jpeg"
+import bewerbungsbild from "../../../resources/portrait/bewerbungsbild.jpg";
+import portrait_two from "../../../resources/portrait/portrait_two.png";
+import huettenbild from "../../../resources/portrait/huette.jpeg";
+import office from "../../../resources/portrait/freizeit.jpeg";
 
 interface AboutmePortraitProps {
     dataFromChild: any;
 }
 
 const AboutmePortrait: React.FC<AboutmePortraitProps> = ({ dataFromChild }) => {
+    useEffect(() => {
+        const images = [bewerbungsbild, portrait_two, huettenbild, office];
+        images.forEach((src) => {
+            const img = new Image();
+            img.src = src;
+        });
+    }, []);
 
     let imgSrc = '';
 
@@ -25,27 +31,22 @@ const AboutmePortrait: React.FC<AboutmePortraitProps> = ({ dataFromChild }) => {
             imgSrc = office;
             break;
         default:
-            imgSrc = 'About';
+            imgSrc = portrait_two;
             break;
     }
 
-    console.log(dataFromChild);
-
     return (
-        <div className={"aboutme_portrait_container"}>
-            <div className={"img-container"}>
-                <div>
-                    <img src={imgSrc} alt="Bild" />
-                </div>
+        <div className="aboutme_portrait_container">
+            <div className="img-container">
+                <img src={imgSrc} alt="Bild" />
             </div>
-            <div className={"portrait_container_input"}>
-                <p className={"portrait_name"}>Johannes Weihele</p>
-                <p className={"portrait_position"}>- M.Sc. Informatik - Software-Engineering -</p>
-                <p className={"portrait_title"}><b>Web- und Software-Engineer</b></p>
+            <div className="portrait_container_input">
+                <p className="portrait_name">Johannes Weihele</p>
+                <p className="portrait_position">- M.Sc. Informatik - Software-Engineering -</p>
+                <p className="portrait_title"><b>Web- und Software-Engineer</b></p>
             </div>
         </div>
     );
 };
 
-
-export default AboutmePortrait
+export default AboutmePortrait;
